731b89a22773759389da16fc9e66ea340066c0e8
Nguyễn Văn Hoàng Nhân <nhan053qt@gmail.com>
2024-01-04 13:29:49 +0700

    -

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,139 +1,139 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-import { getSlug } from '../src/helpers/string';
+// import { getSlug } from '../src/helpers/string';
 
-import {
-  seedAdmins,
-  seedCategories,
-  seedEditors,
-  seedMedias,
-  seedPosts,
-  seedVisits,
-} from './seedData';
-const prisma = new PrismaClient();
+// import {
+//   seedAdmins,
+//   seedCategories,
+//   seedEditors,
+//   seedMedias,
+//   seedPosts,
+//   seedVisits,
+// } from './seedData';
+// const prisma = new PrismaClient();
 
-async function main() {
-  console.log('SEED - START');
+// async function main() {
+//   console.log('SEED - START');
 
-  console.log('SEED - Delete all category and user');
-  await prisma.category.deleteMany({});
-  await prisma.user.deleteMany({});
+//   console.log('SEED - Delete all category and user');
+//   await prisma.category.deleteMany({});
+//   await prisma.user.deleteMany({});
 
-  console.log('SEED -Seed category');
-  await prisma.$transaction([
-    ...seedCategories.map(({ name, thumbnail, slug }) =>
-      prisma.category.upsert({
-        where: { slug: slug },
-        update: {},
-        create: {
-          slug,
-          name,
-          thumbnail,
-          createdAt: new Date(),
-        },
-      }),
-    ),
-  ]);
+//   console.log('SEED -Seed category');
+//   await prisma.$transaction([
+//     ...seedCategories.map(({ name, thumbnail, slug }) =>
+//       prisma.category.upsert({
+//         where: { slug: slug },
+//         update: {},
+//         create: {
+//           slug,
+//           name,
+//           thumbnail,
+//           createdAt: new Date(),
+//         },
+//       }),
+//     ),
+//   ]);
 
-  console.log('SEED - Seed accounts');
-  await prisma.$transaction(
-    [...seedAdmins, ...seedEditors].map(
-      (
-        {
-          email,
-          username,
-          name,
-          role,
-          avatarUrl,
-          password,
-          isVerified,
-          verifiedAt,
-        },
-        index,
-      ) => {
-        return prisma.user.upsert({
-          where: { id: index + 1 },
-          update: {},
-          create: {
-            id: index + 1,
-            email,
-            username,
-            name,
-            role,
-            avatarUrl,
-            password,
-            isVerified,
-            verifiedAt,
-          },
-        });
-      },
-    ),
-  );
+//   console.log('SEED - Seed accounts');
+//   await prisma.$transaction(
+//     [...seedAdmins, ...seedEditors].map(
+//       (
+//         {
+//           email,
+//           username,
+//           name,
+//           role,
+//           avatarUrl,
+//           password,
+//           isVerified,
+//           verifiedAt,
+//         },
+//         index,
+//       ) => {
+//         return prisma.user.upsert({
+//           where: { id: index + 1 },
+//           update: {},
+//           create: {
+//             id: index + 1,
+//             email,
+//             username,
+//             name,
+//             role,
+//             avatarUrl,
+//             password,
+//             isVerified,
+//             verifiedAt,
+//           },
+//         });
+//       },
+//     ),
+//   );
 
-  console.log('SEED - Seed media');
-  await prisma.$transaction([
-    ...seedMedias.map(({ createdAt, fileName }, index) =>
-      prisma.media.upsert({
-        where: { id: index + 1 },
-        update: {},
-        create: {
-          fileName,
-          createdAt,
-        },
-      }),
-    ),
-  ]);
+//   console.log('SEED - Seed media');
+//   await prisma.$transaction([
+//     ...seedMedias.map(({ createdAt, fileName }, index) =>
+//       prisma.media.upsert({
+//         where: { id: index + 1 },
+//         update: {},
+//         create: {
+//           fileName,
+//           createdAt,
+//         },
+//       }),
+//     ),
+//   ]);
 
-  console.log('SEED - Seed posts');
-  await prisma.$transaction([
-    prisma.post.deleteMany({}),
-    ...seedPosts.map(
-      (
-        { title, body, userId, secondaryText, status, createdAt, categoryId },
-        index,
-      ) =>
-        prisma.post.create({
-          data: {
-            id: index + 1,
-            thumbnailMediaId: 1,
-            title,
-            slug: getSlug(title, '-'),
-            body,
-            secondaryText,
-            userId,
-            status,
-            createdAt,
-            categoryId,
-          },
-        }),
-    ),
-  ]);
+//   console.log('SEED - Seed posts');
+//   await prisma.$transaction([
+//     prisma.post.deleteMany({}),
+//     ...seedPosts.map(
+//       (
+//         { title, body, userId, secondaryText, status, createdAt, categoryId },
+//         index,
+//       ) =>
+//         prisma.post.create({
+//           data: {
+//             id: index + 1,
+//             thumbnailMediaId: 1,
+//             title,
+//             slug: getSlug(title, '-'),
+//             body,
+//             secondaryText,
+//             userId,
+//             status,
+//             createdAt,
+//             categoryId,
+//           },
+//         }),
+//     ),
+//   ]);
 
-  console.log('SEED - Seed visits');
-  await prisma.$transaction([
-    ...seedVisits.map(({ postId, userId, percentage, IP }, index) =>
-      prisma.visit.create({
-        data: {
-          id: index + 1,
-          postId,
-          userId,
-          IP,
-          percentage,
-          visitAt: new Date(),
-        },
-      }),
-    ),
-  ]);
+//   console.log('SEED - Seed visits');
+//   await prisma.$transaction([
+//     ...seedVisits.map(({ postId, userId, percentage, IP }, index) =>
+//       prisma.visit.create({
+//         data: {
+//           id: index + 1,
+//           postId,
+//           userId,
+//           IP,
+//           percentage,
+//           visitAt: new Date(),
+//         },
+//       }),
+//     ),
+//   ]);
 
-  console.log('Done seeding');
-}
+//   console.log('Done seeding');
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
